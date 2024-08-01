df8f54a2136c933b6c2d03bcdc0ba6feeb5b581a
PuFh <137668343+PuFh@users.noreply.github.com>
2024-08-01 16:13:18 -0400

    update

--- treino/hora do dia/JS/script.js
+++ treino/hora do dia/JS/script.js
@@ -1,19 +1,24 @@
 function carregar() {
-    var msg = window.document.getElementById('msg')
-    var foto = window.document.getElementById('foto')
+    var msg = window.document.getElementById('imsg')
+    var foto = window.document.querySelector('img#ifoto')
 
     var data = new Date()
     var hora = data.getHours()
+    
     msg.innerHTML = `Agora são ${hora} Horas`
 
     if (hora >= 0 && hora < 12){
         //BOM DIA
-        img.src = 'manha.jpg'
-    }else if (hora >= 12 && hora < 18){
+        foto.src = 'manha.jpg'
+        //esse foto.src é a VARIÁVEL
+        document.body.style.background = '#a07d1f'
+    }else if (hora >=12 && hora < 18 ){
         //BOA TARDE
-        img.src = 'ma'
+        foto.src = 'tarde.jpg'
+        document.body.style.background = '#c04b3c'
     }else{
         //BOA NOITE
-        img.src = 'noite.jpg'
+        foto.src = 'noite.jpg'
+       document.body.style.background = '#2f2f4b'
     }
 }
